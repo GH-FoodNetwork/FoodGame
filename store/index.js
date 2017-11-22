8ce86bd84c70b15ef53a8a50b1475aaff606d8f4
Layla Hedges <layla.hedges@gmail.com>
2017-11-22 10:59:37 -0500

    added money reducer

--- store/index.js
+++ store/index.js
@@ -2,9 +2,11 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import destinations from './destinations';
+import availabilities from './availabilities';
+import money from './money';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({ destinations });
+const reducer = combineReducers({ destinations, availabilities, money });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
