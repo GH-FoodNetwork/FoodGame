4cff0eb75859b7559fc0e44a2efb6af66bd9c61e
Cecilia Chang <ceciliafchang@gmail.com>
2017-11-28 17:31:49 -0500

    format single-recipe

--- views/single-recipe.js
+++ views/single-recipe.js
@@ -2,28 +2,90 @@ import { Container, Graphics, Sprite } from 'pixi.js';
 import { renderer, singleRecipeStage, gameStage, recipeBookStage } from '../main';
 import { setup, textSetup, objectAtlas } from '../atlases';
 
-
 export default function singleRecipe() {
   document.body.appendChild(renderer.view);
 
-  const recipes = setup(singleRecipeStage, objectAtlas.recipeBookInterior, { x: 20, y: 50 }, { x: 0.53, y: 0.53 });
-  //recipes.anchor.set(0);
+  const recipes = setup(
+    singleRecipeStage,
+    objectAtlas.recipeBookInterior,
+    { x: 20, y: 50 },
+    { x: 0.53, y: 0.53 },
+  );
 
   const spacingX = window.innerWidth / 2;
-  const jollofText = textSetup(singleRecipeStage, 'Jollof Rice\nIngredients:\nCooked Rice\nTomato Paste', { x: spacingX, y: 100 });
+  const spacingY = 100;
 
+  const jollofText = textSetup(singleRecipeStage, 'Jollof Rice', { x: spacingX, y: spacingY });
 
-  const jollofLink = textSetup(singleRecipeStage, 'Link To Full Recipe', { x: spacingX, y: 200 });
+  const rice = setup(
+    singleRecipeStage,
+    objectAtlas.cookedRice,
+    { x: spacingX - 150, y: spacingY + 75 },
+    { x: 1, y: 1 },
+  );
+  const plus = textSetup(singleRecipeStage, '+', { x: spacingX - 75, y: spacingY + 75 });
+  const tomato = setup(
+    singleRecipeStage,
+    objectAtlas.tomatoPaste,
+    { x: spacingX, y: spacingY + 75 },
+    { x: 1, y: 1 },
+  );
+  const equals = textSetup(singleRecipeStage, '=', { x: spacingX + 75, y: spacingY + 75 });
+  const jollof = setup(
+    singleRecipeStage,
+    objectAtlas.jollof,
+    { x: spacingX + 150, y: spacingY + 75 },
+    { x: 0.2, y: 0.2 },
+  );
+
+  const step1 = textSetup(singleRecipeStage, 'Step 1: Pick up ingredients from Sous Chef', {
+    x: spacingX,
+    y: spacingY + 150,
+  });
+  const step2 = textSetup(singleRecipeStage, 'Step 2: Take ingredients to mixing station', {
+    x: spacingX,
+    y: spacingY + 180,
+  });
+  const step3 = textSetup(
+    singleRecipeStage,
+    'Step 3: When timer is up, retrieve mixed ingredients',
+    {
+      x: spacingX,
+      y: spacingY + 210,
+    },
+  );
+  const step4 = textSetup(singleRecipeStage, 'Step 4: Take ingredients to frying station', {
+    x: spacingX,
+    y: spacingY + 240,
+  });
+  const step5 = textSetup(singleRecipeStage, 'Step 5: When timer is up, retrieve finished dish', {
+    x: spacingX,
+    y: spacingY + 270,
+  });
+  const step6 = textSetup(singleRecipeStage, 'Step 6: Serve finished dish to customer', {
+    x: spacingX,
+    y: spacingY + 300,
+  });
+
+  const jollofLink = textSetup(singleRecipeStage, 'Link To Full Recipe', {
+    x: spacingX,
+    y: spacingY + 375,
+  });
   jollofLink.interactive = true;
   jollofLink.buttonMode = true;
   jollofLink.on('pointerdown', linkToRecipe);
 
   function linkToRecipe() {
     //window.location.href = 'https://www.epicurious.com/recipes/food/views/african-jollof-rice';
-    window.open('http://www.allnigerianrecipes.com/rice/nigerian-jollof-rice.html', '_blank');
+    window.open('http://www.vegannigerian.com/2013/02/jollof-rice.html', '_blank');
   }
 
-  const arrow = setup(singleRecipeStage, objectAtlas.backArrow, { x: 50, y: 500 }, { x: 0.2, y: 0.2 });
+  const arrow = setup(
+    singleRecipeStage,
+    objectAtlas.backArrow,
+    { x: 50, y: 500 },
+    { x: 0.2, y: 0.2 },
+  );
 
   arrow.interactive = true;
   arrow.buttonMode = true;
